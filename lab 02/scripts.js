2e7602c9ca3bcb8a5461d8fc971e93fa859d2f34
nafisbhamjee <P17157542@my365.dmu.ac.uk>
2021-02-01 15:23:48 +0000

    Commit for Lab 2

---
+++ lab 02/scripts.js
@@ -0,0 +1,9 @@
+function greetMe() {
+  console.log("Hello!!");
+}
+
+function text() {
+  console.log("text content");
+}
+myTrigger.addEventListener('click', text);
+myTrigger.addEventListener('click',text);
